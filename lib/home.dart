7665da48d12984cc57e32aa302c7d29401863d35
timotheegarot <timotheegarot@hotmail.com>
2023-02-11 19:47:23 +0100

    Ajout boutons modifs

--- lib/home.dart
+++ lib/home.dart
@@ -64,7 +64,7 @@ class _HomeState extends State<Home>{
             fontSize: 18
           ),
         ),
-        //trailing: const Icon(Icons.manage_accounts, size: 40),
+        trailing: const Icon(Icons.manage_accounts, size: 40),
       ),
     );
   }
